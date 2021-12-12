6a69a98eba91b92cf75fcfcbfe429303fc950d65
mehmetcelik1705 <mehmetcelik1705@gmail.com>
2021-12-12 15:20:44 +0300

    SQL repository oluşturuldu

---
+++ SQL_session1.sql
@@ -0,0 +1,13 @@
+SELECT name FROM tracks;
+select Composer,name from tracks;
+SELECT * from tracks;
+SELECT DISTINCT Composer from tracks;
+SELECT DISTINCT AlbumId,MediaTypeId from tracks;
+SELECT name from tracks where Composer = "Jimi Hendrix";
+select * 
+from invoices
+where total > 10;
+
+
+
+
