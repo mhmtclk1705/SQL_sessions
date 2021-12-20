93218149405c6aa2aa47f7952041db55f2d0f5e2
mehmetcelik1705 <mehmetcelik1705@gmail.com>
2021-12-20 20:30:29 +0300

    iki tane ödev sorusu yapıldı

--- ödev.sql
+++ ödev.sql
@@ -1,9 +1,13 @@
 	/* invoices tablosunda kaç adet farklı yıl olduğunu hesaplayan sorguyu yazınız*/ 
 
-
+SELECT DISTINCT strftime('%Y',InvoiceDate)
+FROM invoices;
 
 
     /* invoices  tablosunda 2009 ile 2013 tarihileri arasındaki faturaların toplam değerini listeyiniz */
     
-    
+   SELECT sum(Total) as total_amount,
+   InvoiceDate
+   FROM invoices
+   WHERE InvoiceDate BETWEEN '2009-01-01' and '2014-01-01'; 
 
